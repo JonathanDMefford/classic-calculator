0780501d979776259d6be071ce7a15a148444f1b
Jonathan Mefford <Jonathanmefford@Justin-iPhone.localdomain>
2020-03-12 17:00:06 -0400

    show calc

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let app = document.getElementById('app');
-app.className = 'container';
+app.className = 'container my-5 mx-auto';
 
 
 class Element {
@@ -16,29 +16,59 @@ class PageView {
     constructor() { }
 
     buildCalculator() {
+        let chars = ['7', '8', '9', '*', '4', '5', '6', '-', '1', '2', '3', '+'];
+
+        var calRow = new Element('div', 'row', '', '');
+        var calCol1 = new Element('div', 'col-4', '', '');
+        var calCol2 = new Element('div', 'col-4 bg-light', 'calculator' , '');
+        var calCol3 = new Element('div', 'col-4', '' ,'');
+
+        var outputRow = new Element('div', 'row', '', '');
+
+        var outputCol = new Element('div', 'col-12 pr-5 bg-light display-3 text-right text-dark', 'output', '0');
+        outputRow.element.appendChild(outputCol.element);
+        
         var row1 = new Element('div', 'row', 'row1', '');
-        var col1 = new Element('div', 'col-6', 'clear', 'Clear');
-        var col2 = new Element('div', 'col-3', '', '');
-        var col3 = new Element('div', 'col-3', 'divide', '/');
-        let numbers = 1;
-        var mainRow = new Element('div', 'row', '', '');
-        for (let i = 0; i < 9; i++) {
-            var mainCol = new Element('div', 'col-9', '', numbers);
-            numbers++;
+        
+        var col1 = new Element('button', 'col-6 rounded-circle bg-dark p-2 text-white text-center', 'clear', 'Clear');
+        row1.element.appendChild(col1.element);
+        
+        var col2 = new Element('button', 'col-3 rounded-circle p-2 bg-dark text-white', '', '');
+        row1.element.appendChild(col2.element);
+        
+        var col3 = new Element('button', 'col-3 rounded-circle p-2 bg-dark text-white', 'divide', '/');
+        row1.element.appendChild(col3.element);
+        
+        for (let i = 0; i < 3; i++) {
+            var mainRow = new Element('div', 'row', '', '');
+            for (let j = 0; j < 12; j++) {
+                var mainCol = new Element('button', 'col-3 rounded-circle p-2 bg-dark text-white', '', chars[j]);
+                mainRow.element.appendChild(mainCol.element);
+            }
         }
-    
-        row1.Element.appendChild(col1.Element);
-        row1.Element.appendChild(col2.Element);
-        row1.Element.appendChild(col3.Element);
-        app.appendChild(row1.Element);
-    
-    
+
+        var lrow = new Element('div', 'row', '', '');
+
+        var lcol1 = new Element('button', 'col-6 rounded-circle p-2 bg-dark text-white', '0', '0');
+        lrow.element.appendChild(lcol1.element);
+
+        var lcol2 = new Element('button', 'col-3 rounded-circle p-2 bg-dark text-white', 'decimal', '.');
+        lrow.element.appendChild(lcol2.element);
+
+        var lcol3 = new Element('button', 'col-3 rounded-circle p-2 bg-dark text-white', 'equals', '=');
+        lrow.element.appendChild(lcol3.element);
+        
+        
+        calCol2.element.appendChild(outputRow.element);
+        calCol2.element.appendChild(row1.element);
+        calCol2.element.appendChild(mainRow.element);
+        calCol2.element.appendChild(lrow.element);
+        calRow.element.appendChild(calCol1.element);
+        calRow.element.appendChild(calCol2.element);
+        calRow.element.appendChild(calCol3.element);
+        app.appendChild(calRow.element);
     }
-
 }
-
-
-
 
 
 var calc = new PageView()
