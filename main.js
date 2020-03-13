6b2a6dbe8194492423481acd562688a1dbc8b7f6
Jonathan Mefford <Jonathanmefford@Justin-iPhone.localdomain>
2020-03-13 15:59:05 -0400

    buttons

--- main.js
+++ main.js
@@ -13,7 +13,9 @@ class Element {
 
 
 class PageView {
-    constructor() { }
+    constructor(controller) {
+        this.controller = controller;
+    }
 
     buildCalculator() {
         // let ids = ['7', '8', '9', 'multiply', '4', '5', '6', 'subtract', '1', '2', '3', 'add'];
@@ -21,55 +23,55 @@ class PageView {
 
         var calRow = new Element('div', 'row', 'calcrow', '');
         var calCol1 = new Element('div', 'col-4', '', '');
-        var calCol2 = new Element('div', 'col-4 border border-dark bg-light', 'calculator' , '');
-        var calCol3 = new Element('div', 'col-4', '' ,'');
+        var calCol2 = new Element('div', 'col-4 border border-dark bg-light', 'calculator', '');
+        var calCol3 = new Element('div', 'col-4', '', '');
 
         var outputRow = new Element('div', 'row pt-2', '', '');
 
         var outputCol = new Element('div', 'col-12 pr-2 bg-light display-3 text-right text-dark', 'output', '0');
         outputRow.element.appendChild(outputCol.element);
-        
+
         var row1 = new Element('div', 'row px-2 pt-2', 'row1', '');
-        
+
         var col1 = new Element('button', 'col-6 rounded-circle bg-dark p-2 text-white text-center', 'clear', 'Clear');
-        col1.element.addEventListener('click', this.CalcController.handleClicks().bind(this));
+        col1.element.addEventListener('click', this.controller.handleClicks.bind(this.controller));
         row1.element.appendChild(col1.element);
-        
+
         var col2 = new Element('button', 'col-3 rounded-circle p-2 bg-dark text-white', '', '');
-        col2.element.addEventListener('click', this.CalcController.handleClicks().bind(this));
+        col2.element.addEventListener('click', this.controller.handleClicks.bind(this.controller));
         row1.element.appendChild(col2.element);
-        
+
         var col3 = new Element('button', 'col-3 rounded-circle p-2 bg-dark text-white', 'divide', '/');
-        col3.element.addEventListener('click', this.CalcController.handleClicks().bind(this));
+        col3.element.addEventListener('click', this.controller.handleClicks.bind(this.controller));
         row1.element.appendChild(col3.element);
-        
+
         for (let i = 0; i < 3; i++) {
             var mainRow = new Element('div', 'row px-2', 'mainrow', '');
             for (let j = 0; j < 12; j++) {
                 var mainCol = new Element('button', 'col-3 rounded-circle bg-dark text-white', chars[j], chars[j]);
-                mainCol.element.addEventListener('click', this.CalcController.handleClicks().bind(this));
+                mainCol.element.addEventListener('click', this.controller.handleClicks.bind(this.controller));
                 mainRow.element.appendChild(mainCol.element);
                 // bind the click event to the mainCol
                 // bind it to the Controller's handleEvent
-    
+
             }
         }
 
         var lrow = new Element('div', 'row px-2 pb-2', 'lrow', '');
 
         var lcol1 = new Element('button', 'col-6 rounded-circle p-2 bg-dark text-white', '0', '0');
-        lcol1.element.addEventListener('click', this.CalcController.handleClicks().bind(this));
+        lcol1.element.addEventListener('click', this.controller.handleClicks.bind(this.controller));
         lrow.element.appendChild(lcol1.element);
 
         var lcol2 = new Element('button', 'col-3 rounded-circle p-2 bg-dark text-white', 'decimal', '.');
-        lcol2.element.addEventListener('click', this.CalcController.handleClicks().bind(this));
+        lcol2.element.addEventListener('click', this.controller.handleClicks.bind(this.controller));
         lrow.element.appendChild(lcol2.element);
 
         var lcol3 = new Element('button', 'col-3 rounded-circle p-2 bg-dark text-white', 'equals', '=');
-        lcol3.element.addEventListener('click', this.CalcController.handleClicks().bind(this));
+        lcol3.element.addEventListener('click', this.controller.handleClicks.bind(this.controller));
         lrow.element.appendChild(lcol3.element);
-        
-        
+
+
         calCol2.element.appendChild(outputRow.element);
         calCol2.element.appendChild(row1.element);
         calCol2.element.appendChild(mainRow.element);
@@ -79,31 +81,51 @@ class PageView {
         calRow.element.appendChild(calCol3.element);
         app.appendChild(calRow.element);
     }
-}
 
-
-class CalcController {
-    constructor() {}
-
-    handleClicks()
-
-    // need a handleEvent method that accepts the click event
-    // need to tell the Model to do something like calculate
-}
-
-
-// need a model class to mangage state that stores the input and tell the view to update
-// the controller needs store a reference to the model to process the input
-
-
-class Model {
-    constructor() {
-
-
+    updateDisplay() {
+        document.getElementById('output').innerHTML = this.controller.model.curr_button;
     }
 }
 
 
+class CalcController {
+    constructor(model) {
+        this.model = model
+    }
 
-var calc = new PageView()
-calc.buildCalculator()
+    handleClicks(e) {
+        console.log(this)
+        // console.log(buttons);
+        this.model.curr_button = e.target.textContent;
+        this.model.displayButtons();
+    }
+
+    // need a handleEvent method that accepts the click event
+}
+
+
+
+class Model {
+    constructor(view) {
+        this.curr_button = "0"
+        this.view = null
+    }
+
+    setView(view) {
+        this.view = view;
+    }
+
+    displayButtons() {
+        this.view.updateDisplay();
+    }
+
+
+}
+
+
+var model = new Model();
+var controller = new CalcController(model);
+var calc = new PageView(controller);
+model.setView(calc)
+calc.buildCalculator();
+model.displayButtons();
